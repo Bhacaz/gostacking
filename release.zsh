d37d7cc8b2bccd66641cac9455dc24d0fab770b4
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-02-27 17:08:28 -0500

    Update script

--- release.zsh
+++ release.zsh
@@ -7,6 +7,6 @@ sed -i '' "s/Version: \".*\"/Version: \"$version\"/" cmd/root.go
 git add .
 git commit -m "Release $version"
 git tag -a "$version" -m "Release $version"
-git push origin --tags
+git push --tags
 
 goreleaser release
