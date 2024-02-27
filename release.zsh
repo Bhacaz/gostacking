55f8fe81f72d19ee74e59661f1387a546146cca8
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-02-27 17:01:52 -0500

    Update

--- release.zsh
+++ release.zsh
@@ -4,9 +4,9 @@ version=$(<.version)
 
 sed -i '' "s/Version: \".*\"/Version: \"$version\"/" cmd/root.go
 
-#git add cmd/root.go
-#git commit -m "Release $version"
-#git tag -a "$version" -m "Release $version"
-#git push origin "$version"
-#
-#goreleaser release
+git add cmd/root.go
+git commit -m "Release $version"
+git tag -a "$version" -m "Release $version"
+git push origin "$version"
+
+goreleaser release
