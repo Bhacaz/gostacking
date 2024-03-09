f933c79f2672615ba3c54efb682d23bfcfe582b7
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-03-09 13:43:19 -0500

    Remove print

--- internal/stack/manager.go
+++ internal/stack/manager.go
@@ -165,7 +165,6 @@ func (sm StacksManager) SwitchByName(stackName string) error {
 		}
 	} else {
 		stack, err = sm.stacks.GetStackByName(stackName)
-		fmt.Println(stack, err)
 		if err != nil {
 			return err
 		}
