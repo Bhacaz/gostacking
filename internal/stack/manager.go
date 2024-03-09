cc9cd7d0c33d3a181356ffc943cf77e58ef57c02
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-03-09 13:18:53 -0500

    Fix divergance

--- internal/stack/manager.go
+++ internal/stack/manager.go
@@ -380,9 +380,9 @@ func colorFunc(i int) func(...interface{}) string {
 	var branchColorsSequence []func(...interface{}) string
 	branchColorsSequence = append(branchColorsSequence, color.Red)
 	branchColorsSequence = append(branchColorsSequence, color.Purple)
-	branchColorsSequence = append(branchColorsSequence, color.Teal)
 	branchColorsSequence = append(branchColorsSequence, color.Magenta)
 	branchColorsSequence = append(branchColorsSequence, color.Green)
+	branchColorsSequence = append(branchColorsSequence, color.Teal)
 
 	return branchColorsSequence[i%len(branchColorsSequence)]
 }
@@ -391,7 +391,7 @@ func pipesColors(index int, withDivergence bool) string {
 	var result string
 	for i := 0; i < index; i++ {
 		colorF := colorFunc(i)
-		if i == 0 && withDivergence {
+		if i == index-1 && withDivergence {
 			result += colorF("|\\\n")
 		} else {
 			result += colorF("| ")
