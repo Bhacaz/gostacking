9e4dd7533182f6fd3bd69d75655f1f2aace4b55a
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-03-08 19:22:20 -0500

    Add symbols in status

---
+++ internal/stack/branch_status.go
@@ -0,0 +1,36 @@
+package stack
+
+import "github.com/Bhacaz/gostacking/internal/color"
+
+type branchStatus struct {
+	BehindRemote        bool
+	AheadRemote         bool
+	HasDiff             bool
+	BehindDefaultBranch bool
+}
+
+func defaultBranchStatus() branchStatus {
+	return branchStatus{
+		BehindRemote:        false,
+		AheadRemote:         false,
+		HasDiff:             false,
+		BehindDefaultBranch: false,
+	}
+}
+
+func (bs branchStatus) Symbols() string {
+	var symbolsToDisplay string
+	if bs.BehindRemote {
+		symbolsToDisplay += color.Teal("↓")
+	}
+	if bs.AheadRemote {
+		symbolsToDisplay += color.Teal("↑")
+	}
+	if bs.HasDiff {
+		symbolsToDisplay += color.Red("*")
+	}
+	if bs.BehindDefaultBranch {
+		symbolsToDisplay += color.Magenta("*")
+	}
+	return symbolsToDisplay
+}
